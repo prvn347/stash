2d2b413f66987c55d91ff7101416d5ce57274f5c
Your New Username <sahupravin960@gmail.com>
2024-04-02 13:18:23 +0530

    added zod,nextauth,recoil and appbar component

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,5 +1,12 @@
 import { PrismaClient } from "@repo/db/client";
 
+import { useB } from "@repo/store/useB";
+import Balence from "../components/Balence";
 export default function Page(): JSX.Element {
-  return <div className=" text-xl text-red-300">hello</div>;
+  return (
+    <div className=" text-xl text-red-300">
+      hello
+      <Balence />
+    </div>
+  );
 }
